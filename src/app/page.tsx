a778b29261351a38a6436e666a9ca8e9585a444a
Augustine Agladima <austinagladima@gmail.com>
2026-02-13 14:02:32 +0100

    bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import LTS from "./components/LTS";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+    <div className="min-h-screen flex items-center justify-center bg-black">
       <LTS />
     </div>
   );
